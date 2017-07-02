c3a927596050fc7e30b4ac7528127c3b0fcba139
smvoigt <smvoigt@users.noreply.github.com>
2017-07-02 14:47:40 +0930

    Added Tank Drive OpMode

---
+++ TeamCode/src/main/java/org/baconeers/opmode/TankDrive.java
@@ -0,0 +1,52 @@
+package org.baconeers.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.baconeers.configurations.EskyBot;
+import org.firstglobal.FgCommon.FGOpMode;
+import org.firstglobal.FgCommon.GamePadMotor;
+
+@TeleOp (group = "Tank Drive")
+//@Disabled
+public class TankDrive extends FGOpMode {
+
+    private EskyBot robot;
+    private GamePadMotor  leftWheel;
+    private GamePadMotor  rightWheel;
+
+
+    /**
+     * Implement this method to define the code to run when the Init button is pressed on the Driver station.
+     */
+    @Override
+    protected void onInit() {
+
+        robot = EskyBot.newConfig(hardwareMap, telemetry);
+
+    }
+
+    /**
+     * Implement this method to define the code to run when the Play button is pressed on the Driver station.
+     * This code will run once.
+     */
+    @Override
+    protected void onStart() throws InterruptedException {
+        super.onStart();
+        leftWheel = new GamePadMotor(this,  gamepad1, robot.driveLeft, GamePadMotor.Control.LEFT_STICK_Y);
+        rightWheel = new GamePadMotor(this,  gamepad1, robot.driveRight, GamePadMotor.Control.RIGHT_STICK_Y);
+    }
+
+    /**
+     * Implement this method to define the code to run when the Start button is pressed on the Driver station.
+     * This method will be called in a loop on each hardware cycle
+     */
+    @Override
+    protected void activeLoop() throws InterruptedException {
+
+        //update the motors with the gamepad joystick values
+        leftWheel.update();
+        rightWheel.update();
+
+    }
+
+}
